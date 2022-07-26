2aae2b8187aaf675f6ac54626b553d37162bb0c5
keertu24 <keerthankeertu24@gmail.com>
2022-07-26 23:27:05 +0530

    login edit

--- upevent/upevent/views.py
+++ upevent/upevent/views.py
@@ -9,9 +9,19 @@ def aboutUs(request):
     return render (request,'upevents/about.html')
 
 def login(request):
+    # user login page
+    return render(request,'upevents/userlogin.html')
+
+def confirmlogin(request):
+    #it will confirm that is authecate user
     return HttpResponse("user login")
 
 def signUp(request):
+    #Just template to show the signup page
+   return render(request,'upevents/usersignup.html')
+
+def confirmsignUp(request):
+    #end point to add a user
     return HttpResponse("user signup")
 
 def organiserlogin(request):
